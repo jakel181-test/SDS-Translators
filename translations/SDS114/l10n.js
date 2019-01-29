ac5554d32091fe4024d47049c946d801280df72e
jakel181 <17256688+jakel181@users.noreply.github.com>
2019-01-29 18:25:12 -0500

    Update l10n.js

--- translations/SDS114/l10n.js
+++ translations/SDS114/l10n.js
@@ -6,4 +6,11 @@ var l10n = {
     "standard":" ",
     "curators":"The SDS is curated by"
   },
+  "an": {
+    "language": "Aragonés",
+    "translator": "Jakel181",
+    "unique": "2121211\n<br/><br/>sdaxswead<br/>",
+    "standard": "drcftvgybhjn\n<br/>fcgvhbjnkm\n<br/>dcftvgybhunji\n<br/>cftvgybhunji\n<br/><br/>drcftvgybhunj\n ctfvyghubnj <a href=\"https://scratch.mit.edu/projects/55738732/\">https://scratch.mit.edu/projects/55738732/</a>\ncfvgybhujnk\n<br/><br/>cfvgbhjnkm <a href=\"https://scratch.mit.edu/studios/93627/\">https://scratch.mit.edu/studios/93627/</a>\n<br/><br/>drcftvgybhujn <a href=\"http://wiki.scratch.mit.edu/wiki/SDS/\">http://wiki.scratch.mit.edu/wiki/SDS/</a>\n<br/><br/>drcfvghbjnkm <a href=\"https://scratch.mit.edu/users/SDS-Updates/\">@SDS-Updates</a>\n<br/><br/>ftvgybhunji",
+    "curators": "xdrcftvghbjnkm"
+  },
 }
